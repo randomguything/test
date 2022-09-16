458c25adc7c3d75aa8ea04c8de8e36ca0562f3de
ESCG\522044 <522044@chrome.escg.ac.uk>
2022-09-16 12:15:06 +0100

    hello world string C#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Program
+{
+    class prog
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("bruh");
+            Console.WriteLine("bye world im out");
+
+
+        }
+
+    }
+
+ 
+}
+
+
